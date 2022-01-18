e0dfd1dbdfb62380c0eccf21e6f8419b09633a0b
Thanosdidnothingrong <64291783+Thanosdidnothingrong@users.noreply.github.com>
2022-01-17 22:17:06 -0500

    Broken Modal Implementation
    Deploys successfully on public/index.html. Breaks when running with React?

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
-
 import React, { useState } from "react";
-import MojsExample from "./MojsExample";
 import Button from "./Button";
 import Burst from "./Burst";
 import "./styles.css";
 
 export default function App() {
-    const [slowMo, setSlowMo] = useState(false);
     return (
         <>
             <burst></burst>
+
             <div className="SplashPage">Hunter Habersaat</div>
             <footer>
                 <p>Connect with me:</p>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import BrokenModal from "./BrokenModal";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    rootElement
-);
+ReactDOM.render(<React.StrictMode></React.StrictMode>, rootElement);
