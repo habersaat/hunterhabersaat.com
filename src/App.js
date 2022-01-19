4ab5e687cc5a9d25fef226b22e49f13e1b381707
HunterHab <hunterhabersaat@gmail.com>
2022-01-19 16:01:00 -0500

    Broken Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import React, { useState } from "react";
 import BottomButton from "./BottomButton";
-import Burst from "./Burst";
 import "./styles.css";
-import BrokenModal from "./BrokenModal";
 
 export default function App() {
     const [slowMo, setSlowMo] = useState(false);
     return (
         <>
-            <div className="SplashPage">Hunter Habersaat</div>
+            <div className="SplashPage">
+            <div class="highlightTextOut">
+                <a alt="HOME">HOME</a>
+                <a alt="ARTICLES">ARTICLES</a>
+                <a alt="PORTFOLIO">PORTFOLIO</a>
+                <a alt="ABOUT">ABOUT</a>
+                <a alt="CONTACT">CONTACT</a>
+            </div>
+                Hunter Habersaat
+                </div>
+            <div className="whoami">This is some filler text for now.</div>
             <footer>
                 <p className="ConnectText">Connect with me:</p>
                 <BottomButton
