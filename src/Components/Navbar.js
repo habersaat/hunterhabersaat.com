a93d2fcb24bf006a1ad102f88731fbac7445678d
Thanosdidnothingrong <64291783+Thanosdidnothingrong@users.noreply.github.com>
2022-01-20 23:00:30 -0500

    Navbar Placement

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,14 +5,12 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <>
-            <div>
-                <div class="highlightTextOut">
-                    <Link to="/">HOME </Link>
-                    <Link to="/articles">ARTICLES </Link>
-                    <Link to="/portfolio">PORTFOLIO </Link>
-                    <Link to="/about">ABOUT </Link>
-                    <Link to="/contact">CONTACT </Link>
-                </div>
+            <div className="highlightTextOut">
+                <Link to="/">HOME </Link>
+                <Link to="/articles">ARTICLES </Link>
+                <Link to="/portfolio">PORTFOLIO </Link>
+                <Link to="/about">ABOUT </Link>
+                <Link to="/contact">CONTACT </Link>
             </div>
         </>
     );
