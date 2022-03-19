553b9e5fd4ca39b96a63e8d6db88403778c2ecb5
Thanosdidnothingrong <hunterhabersaat@gmail.com>
2022-03-18 22:47:29 -0400

    timer added

--- src/pages/NHSSecret.js
+++ src/pages/NHSSecret.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import React, { useState, Component } from "react";
+import React, { useState, useEffect, Component } from "react";
 import BottomButton from "../BottomButton";
 import "../styles.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -7,25 +7,45 @@ import Navbar from "../Components/Navbar";
 import Typewriter from "typewriter-effect";
 
 export default function NHSSecret() {
+    const [timer, setTimer] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (timer <= 22) {
+                setTimer(timer + 1);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [timer]);
+
     return (
         <>
             <div className="nhssnakegame">
                 <div className="secrettext">
-                    <Typewriter
-                        onInit={(typewriter) => {
-                            typewriter
-                                .typeString("You found the secret...")
-                                .pauseFor(2500)
-                                .deleteChars(23)
-                                .typeString("Beat the game, win a prize.")
-                                .pauseFor(2500)
-                                .deleteChars(27)
-                                .typeString(
-                                    "<span>Obtain a score of at least 100.</span>"
-                                )
-                                .start();
-                        }}
-                    />
+                    {timer <= 22 ? (
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString("You found the secret...")
+                                    .pauseFor(2500)
+                                    .deleteChars(23)
+                                    .typeString("Beat the game, win a prize.")
+                                    .pauseFor(2500)
+                                    .deleteChars(27)
+                                    .typeString(
+                                        "Obtain a score of at least 100."
+                                    )
+                                    .start();
+                            }}
+                        />
+                    ) : (
+                        <>
+                            <span className="staticsecrettext">
+                                Obtain a score of at least 100.
+                            </span>
+                        </>
+                    )}
                 </div>
                 <div className="snakegame">
                     <iframe
